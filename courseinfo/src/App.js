85bfa719cc2736c2a1b74bf15f2b4adf1509a4a8
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-01 23:20:21 +0430

    2.5 courseinfo Step10 separate module

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,43 +1,4 @@
-const Header = ({ course }) => (
-  <div>
-    <br /> <h2>{course}</h2>
-  </div>
-);
-const Total = ({ parts }) => (
-  <p>
-    <b>
-      total of {parts.map((p) => p.exercises).reduce((a, cv) => a + cv)}{' '}
-      exercises
-    </b>
-  </p>
-);
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((p, i) => (
-      <Part key={i} name={p.name} exercises={p.exercises} />
-    ))}
-  </>
-);
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
-const Courses = ({ courses }) => (
-  <div>
-    {courses.map((course) => (
-      <Course course={course} />
-    ))}
-  </div>
-);
+import Courses from './components/Courses';
 const App = () => {
   const courses = [
     {
