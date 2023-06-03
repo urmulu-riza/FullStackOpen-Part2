9204d82fddbd1107bf370aa68621d23e404e845c
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-03 21:33:43 +0430

    2.11 phonebook Step6 json-server axios

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Form from './components/Form';
 import Numbers from './components/Number';
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
+  // [{ name: 'Arto Hellas', number: '040-123456' },
+  // { name: 'Ada Lovelace', number: '39-44-5323523' },
+  // { name: 'Dan Abramov', number: '12-43-234345' },
+  // { name: 'Mary Poppendieck', number: '39-23-6423122' },]
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filtered, setFiltered] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleChange = (e, type) => {
     switch (type) {
